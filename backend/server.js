41d24b4c19df1aeb8430309192b93d222f6727e4
Rishi karir <rishikarir0@example.com>
2026-01-03 16:09:43 +0530

    commit backend

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,8 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "https://primetrade-taskmgmt.netlify.app"}));
 app.use(express.json());
-
 
 
 
@@ -119,10 +118,11 @@ app.delete("/api/tasks/:id", authMiddleware, (req, res) => {
   res.json({ message: "Task deleted" });
 });
 
-// ==================
-// Start Server
-// ==================
-const PORT = 5000;
+
+
+const PORT = process.env.PORT || 5000;
+
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
